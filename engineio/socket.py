7547fa997b8428f5fa9f1873daafedc82a7f0bb8
mt3925 <mt3925@sina.com>
2017-06-02 16:33:54 +0800

    Update socket.py

--- engineio/socket.py
+++ engineio/socket.py
@@ -141,7 +141,7 @@ class Socket(object):
         # try to set a socket timeout matching the configured ping interval
         for attr in ['_sock', 'socket']:  # pragma: no cover
             if hasattr(ws, attr) and hasattr(getattr(ws, attr), 'settimeout'):
-                getattr(ws, attr).settimeout(self.server.ping_timeout)
+                getattr(ws, attr).settimeout(self.server.ping_timeout + 1)
 
         if self.connected:
             # the socket was already connected, so this is an upgrade
